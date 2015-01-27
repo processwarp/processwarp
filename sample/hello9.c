fb6ec5c7633d835c43b7c517fb4302f8176aead1
Yuji Ito <llamerada.jp@gmail.com>
2015-01-27 16:15:11 +0900

    hello9の内容を見直し refs #17

--- sample/hello9.c
+++ sample/hello9.c
@@ -1,34 +1,34 @@
 
 #include <stdio.h>
 
-char* hello1 = "hello1\n";
-const char* hello2 = "hello2\n";
-static const char* hello3 = "hello3\n";
-char hello4[] = "hello4\n";
-const char hello5[] = "hello5\n";
-static const char hello6[] = "hello6\n";
+char* hello1 = "hello %d\n";
+const char* hello2 = "hello %d\n";
+static const char* hello3 = "hello %d\n";
+char hello4[] = "hello %d\n";
+const char hello5[] = "hello %d\n";
+static const char hello6[] = "hello %d\n";
 
 int main(int argc, char* argv[]) {
-  char* hello7 = "hello7\n";
-  const char* hello8 = "hello8\n";
-  static const char* hello9 = "hello9\n";
-  char hello10[] = "hello10\n";
-  const char hello11[] = "hello11\n";
-  static const char hello12[] = "hello12\n";
+  char* hello7 = "hello %d\n";
+  const char* hello8 = "hello %d\n";
+  static const char* hello9 = "hello %d\n";
+  char hello10[] = "hello %d\n";
+  const char hello11[] = "hello %d\n";
+  static const char hello12[] = "hello %d\n";
 
-  printf("hello0\n");
-  printf(hello1);
-  printf(hello2);
-  printf(hello3);
-  printf(hello4);
-  printf(hello5);
-  printf(hello6);
-  printf(hello7);
-  printf(hello8);
-  printf(hello9);
-  printf(hello10);
-  printf(hello11);
-  printf(hello12);
+  printf("hello %d\n", 0);
+  printf(hello1, 1);
+  printf(hello2, 2);
+  printf(hello3, 3);
+  printf(hello4, 4);
+  printf(hello5, 5);
+  printf(hello6, 6);
+  printf(hello7, 7);
+  printf(hello8, 8);
+  printf(hello9, 9);
+  printf(hello10, 10);
+  printf(hello11, 11);
+  printf(hello12, 12);
 
   printf("%p %s", "hello0\n", "hello0\n");
   printf("%p %s", hello1, hello1);
