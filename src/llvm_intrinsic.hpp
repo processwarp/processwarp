7bbb3a154e50125089f12407ddeb33e629b42a03
Yuji Ito <llamerada.jp@gmail.com>
2015-02-02 19:50:37 +0900

    組み込み関数の追加IFを作成。BitCast命令に対応。戻り値がvoidの関数呼び出しでクラッシュする不具合に対応。alloca命令に対応。memcpyに対応。hello9が動くようになった。

---
+++ src/llvm_intrinsic.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "definitions.hpp"
+
+namespace usagi {
+  /**
+   * LLVMの組み込み関数
+   */
+  class LlvmIntrinsic {
+  public:
+    /**
+     * memcpy関数。
+     * srcから取り出すパラメタは以下のとおり。
+     * vaddr_t dst コピー先。
+     * vaddr_t src コピー元。
+     * T len コピーサイズ。
+     * int32_t align アライメント。
+     * int8_t isvolation 実行順番の制約(VMでは実行順番を入れ替えないので無視する)。
+     */
+    static void memcpy(VMachine& vm, Thread& th, IntrinsicFuncParam p,
+		       vaddr_t dst, std::vector<uint8_t>& src);
+  };
+}
