413b1b9c6132d1dde51d03a38681d13f75bbd36b
Yuji Ito <llamerada.jp@gmail.com>
2015-08-02 19:28:14 +0900

    update Convert

--- src/convert.hpp
+++ src/convert.hpp
@@ -9,129 +9,12 @@
 namespace processwarp {
   namespace Convert {
     /**
-     * Convert process-id from JSON.
-     * @param json JSON.
-     * @return process-id.
+     * Convert integer to string.
+     * @param num A source integer.
+     * @return A integer as string.
      */
-    inline vpid_t json2vpid(const picojson::value& json) {
-      return json.get<vpid_t>();
-    }
-
-    /**
-     * Convert JSON to bool.
-     * @param json Source JSON.
-     * @return A converted integer.
-     */
-    inline bool json2bool(const picojson::value& json) {
-      return json.get<std::string>() == "T";
-    }
-    
-    /**
-     * Convert JSON to integer.
-     * @param json Source JSON.
-     * @return A converted integer.
-     */
-    template<class T> T json2int(const picojson::value& json) {
-      return Util::hex_str2num<T>(json.get<std::string>());
-    }
-
-    /**
-     * Convert a thread-id from JSON.
-     * @param json Source JSON.
-     * @return A thread-id.
-     */
-    inline vtid_t json2vtid(const picojson::value& json) {
-      return json2int<vtid_t>(json);
-    }
-
-    /**
-     * Convert a virtual address from JSON.
-     * @param json Source JSON.
-     * @return A virtual address.
-     */
-    inline vaddr_t json2vaddr(const picojson::value& json) {
-      return json2int<vaddr_t>(json);
-    }
-
-    /**
-     * Convert a instruction code from JSON.
-     * @param json Source JSON.
-     * @return A instruction code.
-     */
-    inline instruction_t json2code(const picojson::value& json) {
-      return json2int<instruction_t>(json);
-    }
-
-    /**
-     * Convert a device-id from JSON
-     * @param json Source JSON.
-     * @return A device-id.
-     */
-    inline dev_id_t json2devid(const picojson::value& json) {
-      return json.get<dev_id_t>();
-    }
-
-    /**
-     * Convert process-id to JSON.
-     * @param pid Source process-id.
-     * @return Process-id as JSON.
-     */
-    inline picojson::value vpid2json(const vpid_t& pid) {
-      return picojson::value(pid);
-    }
-
-    /**
-     * Convert bool to JSON.
-     * @param b Source boolean.
-     * @return Boolean as JSON.
-     */
-    inline picojson::value bool2json(bool b) {
-      return picojson::value(std::string(b ? "T" : "F"));
-    }
-    
-    /**
-     * Convert integer to JSON.
-     * @param num Source integer.
-     * @return Integer as JSON.
-     */
-    template<class T> picojson::value int2json(T num) {
-      return picojson::value(Util::num2hex_str<T>(num));
-    }
-
-    /**
-     * Convert thread-id to JSON.
-     * @param tid Source thread-id.
-     * @return Thread-id as JSON.
-     */
-    inline picojson::value vtid2json(const vtid_t& tid) {
-      return int2json<vtid_t>(tid);
-    }
-
-    /**
-     * Convert virtual address to JSON.
-     * @param addr Source virtual address.
-     * @return Virtual address as JSON.
-     */
-    inline picojson::value vaddr2json(vaddr_t addr) {
-      return int2json<vaddr_t>(addr);
-    }
-
-    /**
-     * Convert instruction code to JSON.
-     * @param code Source instruction code.
-     * @return Instruction code as JSON.
-     */
-    inline picojson::value code2json(instruction_t code) {
-      return int2json<instruction_t>(code);
-    }
-
-    /**
-     * Convert device-id to JSON.
-     * @param dev_id Source device-id.
-     * @return Device-id as JSON.
-     */
-    inline picojson::value devid2json(const dev_id_t& dev_id) {
-      return picojson::value(dev_id);
+    template<class T> std::string int2str(T num) {
+      return Util::num2hex_str<T>(num);
     }
 
     /**
@@ -149,7 +32,7 @@ namespace processwarp {
      * @return thread-id as string.
      */
     inline std::string vtid2str(const vtid_t& tid) {
-      return Util::num2hex_str<vtid_t>(tid);
+      return int2str<vtid_t>(tid);
     }
 
     /**
@@ -158,7 +41,7 @@ namespace processwarp {
      * @return Virtual-address as string.
      */
     inline std::string vaddr2str(const vaddr_t& addr) {
-      return Util::num2hex_str<vaddr_t>(addr);
+      return int2str<vaddr_t>(addr);
     }
 
     /**
@@ -168,6 +51,15 @@ namespace processwarp {
      */
     inline std::string devid2str(const dev_id_t& dev_id) {
       return dev_id;
+    }
+
+    /**
+     * Convert string to integer.
+     * @param json A source string.
+     * @return A converted integer.
+     */
+    template<class T> T str2int(const std::string& str) {
+      return Util::hex_str2num<T>(str);
     }
 
     /**
@@ -185,7 +77,7 @@ namespace processwarp {
      * @param thread-id.
      */
     inline vtid_t str2vtid(const std::string& str) {
-      return Util::hex_str2num<vtid_t>(str);
+      return str2int<vtid_t>(str);
     }
 
     /**
@@ -194,7 +86,7 @@ namespace processwarp {
      * @param Virtual-address.
      */
     inline vaddr_t str2vaddr(const std::string& str) {
-      return Util::hex_str2num<vaddr_t>(str);
+      return str2int<vaddr_t>(str);
     }
 
     /**
@@ -204,6 +96,158 @@ namespace processwarp {
      */
     inline dev_id_t str2devid(const std::string& str) {
       return str;
+    }
+
+    /**
+     * Convert process-id from JSON.
+     * @param json JSON.
+     * @return process-id.
+     */
+    inline vpid_t json2vpid(const picojson::value& json) {
+      return str2vpid(json.get<std::string>());
+    }
+
+    /**
+     * Convert JSON to bool.
+     * @param json Source JSON.
+     * @return A converted integer.
+     */
+    inline bool json2bool(const picojson::value& json) {
+      return json.get<std::string>() == "T";
+    }
+    
+    /**
+     * Convert JSON to integer.
+     * @param json Source JSON.
+     * @return A converted integer.
+     */
+    template<class T> T json2int(const picojson::value& json) {
+      return str2int<T>(json.get<std::string>());
+    }
+
+    /**
+     * Convert a thread-id from JSON.
+     * @param json Source JSON.
+     * @return A thread-id.
+     */
+    inline vtid_t json2vtid(const picojson::value& json) {
+      return str2vtid(json.get<std::string>());
+    }
+
+    /**
+     * Convert a virtual address from JSON.
+     * @param json Source JSON.
+     * @return A virtual address.
+     */
+    inline vaddr_t json2vaddr(const picojson::value& json) {
+      return str2vaddr(json.get<std::string>());
+    }
+
+    /**
+     * Convert a vector of virtual address from JSON.
+     * @param json Source JSON.
+     * @return A vector of virtual address.
+     */
+    inline std::vector<vaddr_t> json2vaddr_vector(const picojson::value& json) {
+      std::vector<vaddr_t> av;
+      for (auto& it : json.get<picojson::array>()) {
+	av.push_back(json2vaddr(it));
+      }
+      return av;
+    }
+
+    /**
+     * Convert a instruction code from JSON.
+     * @param json Source JSON.
+     * @return A instruction code.
+     */
+    inline instruction_t json2code(const picojson::value& json) {
+      return str2int<instruction_t>(json.get<std::string>());
+    }
+
+    /**
+     * Convert a device-id from JSON
+     * @param json Source JSON.
+     * @return A device-id.
+     */
+    inline dev_id_t json2devid(const picojson::value& json) {
+      return str2devid(json.get<std::string>());
+    }
+
+    /**
+     * Convert process-id to JSON.
+     * @param pid Source process-id.
+     * @return Process-id as JSON.
+     */
+    inline picojson::value vpid2json(const vpid_t& pid) {
+      return picojson::value(vpid2str(pid));
+    }
+
+    /**
+     * Convert bool to JSON.
+     * @param b Source boolean.
+     * @return Boolean as JSON.
+     */
+    inline picojson::value bool2json(bool b) {
+      return picojson::value(std::string(b ? "T" : "F"));
+    }
+    
+    /**
+     * Convert integer to JSON.
+     * @param num Source integer.
+     * @return Integer as JSON.
+     */
+    template<class T> picojson::value int2json(T num) {
+      return picojson::value(int2str<T>(num));
+    }
+
+    /**
+     * Convert thread-id to JSON.
+     * @param tid Source thread-id.
+     * @return Thread-id as JSON.
+     */
+    inline picojson::value vtid2json(const vtid_t& tid) {
+      return picojson::value(vtid2str(tid));
+    }
+
+    /**
+     * Convert virtual address to JSON.
+     * @param addr Source virtual address.
+     * @return Virtual address as JSON.
+     */
+    inline picojson::value vaddr2json(vaddr_t addr) {
+      return int2json<vaddr_t>(addr);
+    }
+
+    /**
+     * Convert vector of virtual address to JSON.
+     * @param addr Source vector of virtual address.
+     * @return Virtual address as JSON.
+     */
+    inline picojson::value vaddr_vector2json(const std::vector<vaddr_t> av) {
+      picojson::array json;
+      for (auto& it : av) {
+	json.push_back(vaddr2json(it));
+      }
+      return picojson::value(json);
+    }
+
+    /**
+     * Convert instruction code to JSON.
+     * @param code Source instruction code.
+     * @return Instruction code as JSON.
+     */
+    inline picojson::value code2json(instruction_t code) {
+      return picojson::value(int2str<instruction_t>(code));
+    }
+
+    /**
+     * Convert device-id to JSON.
+     * @param dev_id Source device-id.
+     * @return Device-id as JSON.
+     */
+    inline picojson::value devid2json(const dev_id_t& dev_id) {
+      return picojson::value(devid2str(dev_id));
     }
   }
 }
