3c71d61bc38117e79fd0e51d7f6e7572ded50f11
Yuji Ito <llamerada.jp@gmail.com>
2015-12-02 16:41:29 +0900

    add electron initialise

---
+++ src/electron/main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var ipc = require('electron').ipcMain;
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+/**
+ * Quit main process when all window was closed.
+ * @return {void}
+ */
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+/**
+ * On start application, create new window.
+ */
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Open development and debug tool.
+  // mainWindow.openDevTools(true);
+
+  // Unlink BrowserWindow object when catch closed event.
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
+
+/**
+ * On activate application and not exist window, create windown again.
+ */
+app.on('activate', function() {
+  if (!mainWindow) {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    // Unlink BrowserWindow object when catch closed event.
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+    });
+  }
+});
