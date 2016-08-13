c31755fbd24f30d7b7cfbd72598ef989539ed266
Yuji Ito <llamerada.jp@gmail.com>
2016-08-13 16:49:09 +0900

    apply Symbols module to multi thread

--- src/core/symbols.cpp
+++ src/core/symbols.cpp
@@ -6,18 +6,29 @@
 
 namespace processwarp {
 
-// シンボル文字列を取得する。
+/**
+ * シンボル文字列を取得する。
+ * @return シンボル文字列。
+ */
 const std::string& Symbols::Symbol::str() const {
   return *this;
 }
 
-// 文字列を指定してシンボルを作成。
+/**
+ * 文字列を指定してシンボルを作成。
+ * @param str_ シンボルに対応する文字列
+ */
 Symbols::Symbol::Symbol(const std::string& str) :
     std::string(str) {
 }
 
-// 文字列に対応するシンボル文字列を取得する。
+/**
+ * 文字列に対応するシンボル文字列を取得する。
+ * @param src 文字列
+ * @return シンンボル文字列。
+ */
 const Symbols::Symbol& Symbols::get(const std::string& src) {
+  Lock::Guard guard(mutex);
   // srcで既存のシンボル文字列の集合を捜査する。
   auto exist = store.find(src);
   if (exist != store.end()) {
--- src/core/symbols.hpp
+++ src/core/symbols.hpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <memory>
 
+#include "lock.hpp"
+
 namespace processwarp {
 /**
  * 同一の文字列に対して、同一のインスタンス：シンボルを生成するクラス。
@@ -22,37 +24,21 @@ class Symbols {
  public:
   class Symbol : private std::string {
    public:
-    /**
-     * シンボル文字列を取得する。
-     * @return シンボル文字列。
-     */
     const std::string& str() const;
 
    private:
     friend Symbols;
 
-    /** デフォルトコンストラクタへはアクセス禁止 */
     Symbol();
-
-    /** コピーコンストラクタへはアクセス禁止 */
     explicit Symbol(const Symbol&);
-
-    /**
-     * 文字列を指定してシンボルを作成。
-     * @param str_ シンボルに対応する文字列
-     */
     explicit Symbol(const std::string& str_);
   };
 
-  /**
-   * 文字列に対応するシンボル文字列を取得する。
-   * @param src 文字列
-   * @return シンンボル文字列。
-   */
   const Symbol& get(const std::string& src);
 
  private:
   /** シンボル文字列の集合。 */
   std::map<std::string, std::unique_ptr<Symbol>> store;
+  Lock::Mutex mutex;
 };
 }  // namespace processwarp
