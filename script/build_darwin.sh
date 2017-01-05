efcb18043d3bb7b3f2cef425cc4dd471e5b69008
Yuji Ito <llamerada.jp@gmail.com>
2017-01-05 09:51:34 +0900

    remove submodule because it causes a bit problem

--- script/build_darwin.sh
+++ script/build_darwin.sh
@@ -5,19 +5,17 @@ set -eux
 _pwd=`pwd`
 _root=$(cd $(dirname $0)/.. && pwd)
 
+# Set local environment path.
+if [ -z "${env_path+x}" ] ; then
+    export LOCAL_ENV_PATH=${_root}/local
+fi
+mkdir -p ${LOCAL_ENV_PATH}/src
+
 # Check clang.
 if ! type clang >/dev/null 2>&1; then
     echo "Please install clang before build processwarp."
     exit 1
 fi
-
-# Download submodules
-cd ${_root}
-git submodule init
-git submodule update
-cd ${_root}/lib/socket.io-client-cpp
-git submodule init
-git submodule update
 
 # Install requirement packages.
 if ! type brew >/dev/null 2>&1; then
@@ -38,19 +36,63 @@ if ! type npm >/dev/null 2>&1; then
     brew install node
 fi
 
-# Compile libfluent
-cd ${_root}
-mkdir -p tmp/libfluent
-mkdir -p local
-cd ${_root}/tmp/libfluent
-cmake -DCMAKE_INSTALL_PREFIX:PATH=${_root}/local ${_root}/lib/libfluent
+# Install libfluent
+if [ -e ${LOCAL_ENV_PATH}/src/libfluent ]; then
+    cd ${LOCAL_ENV_PATH}/src/libfluent
+    git fetch
+else
+    cd ${LOCAL_ENV_PATH}/src
+    git clone https://github.com/m-mizutani/libfluent.git
+fi
+cd ${LOCAL_ENV_PATH}/src/libfluent
+git checkout 1be7405
+
+mkdir -p ${LOCAL_ENV_PATH}/tmp/libfluent
+cd ${LOCAL_ENV_PATH}/tmp/libfluent
+cmake -DCMAKE_INSTALL_PREFIX:PATH=${LOCAL_ENV_PATH} ${LOCAL_ENV_PATH}/src/libfluent
 make all install
 # Remove dylib file to avoid dynamic-link because we cannot change DYLD_LIBRARY_PATH.
-rm ${_root}/local/lib/libfluent.dylib
+rm ${LOCAL_ENV_PATH}/lib/libfluent.dylib
+
+# Install picojson
+if [ -e ${LOCAL_ENV_PATH}/src/picojson ]; then
+    cd ${LOCAL_ENV_PATH}/src/picojson
+    git fetch
+else
+    cd ${LOCAL_ENV_PATH}/src
+    git clone https://github.com/kazuho/picojson.git
+fi
+cd ${LOCAL_ENV_PATH}/src/picojson
+git checkout 25fc213
+
+# Install re2
+if [ -e ${LOCAL_ENV_PATH}/src/re2 ]; then
+    cd ${LOCAL_ENV_PATH}/src/re2
+    git fetch
+else
+    cd ${LOCAL_ENV_PATH}/src
+    git clone https://github.com/google/re2.git
+fi
+cd ${LOCAL_ENV_PATH}/src/re2
+git checkout 09fc9ce
+
+# Install socket.io
+if [ -e ${LOCAL_ENV_PATH}/src/socket.io-client-cpp ]; then
+    cd ${LOCAL_ENV_PATH}/src/socket.io-client-cpp
+    git fetch
+else
+    cd ${LOCAL_ENV_PATH}/src
+    git clone https://github.com/socketio/socket.io-client-cpp.git
+fi
+cd ${LOCAL_ENV_PATH}/src/socket.io-client-cpp
+git checkout f02dc68
+git submodule init
+git submodule update
+
 
 # Compile native programes.
 cd ${_root}
-cmake -DCMAKE_BUILD_TYPE=Debug -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include .
+cmake -DCMAKE_BUILD_TYPE=Debug -DLOCAL_ENV_PATH=${LOCAL_ENV_PATH} -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include .
 make
 make install
 make const_electron
